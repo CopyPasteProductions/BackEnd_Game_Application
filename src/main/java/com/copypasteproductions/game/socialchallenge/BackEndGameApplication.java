8e3312c238088ff90c218ddcaaf0e5533ae67713
Tim Britton <ziberath@gmail.com>
2015-09-19 13:21:15 -0500

    Init Spring Boot Commit

---
+++ src/main/java/com/copypasteproductions/game/socialchallenge/BackEndGameApplication.java
@@ -0,0 +1,12 @@
+package com.copypasteproductions.game.socialchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndGameApplication.class, args);
+    }
+}
